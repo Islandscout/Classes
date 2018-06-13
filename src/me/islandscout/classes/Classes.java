5a42f8886e054d1667b5ee5554dce4b5cc6a26b7
Islandscout <islandscout8@gmail.com>
2018-06-12 19:08:31 -0700

    Initial

---
+++ src/me/islandscout/classes/Classes.java
@@ -0,0 +1,16 @@
+package me.islandscout.classes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Classes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Classes enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Classes disabled");
+    }
+}
